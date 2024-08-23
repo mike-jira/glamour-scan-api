aacd0ddfe03280b42d92c68ea9bf3eafa99f919a
mike-jira <mike.jirasontornchai@gmail.com>
2024-08-24 03:09:37 +0700

    test/implement hashPassword & validatePassword on AuthService

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,8 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import * as bcrypt from 'bcrypt';
+
+jest.mock('bcrypt');
 
 describe('AuthService', () => {
   let service: AuthService;
+
+  const password = 'testpassword';
+  const salt = 'salt';
+  const hashedPassword = 'hashedPassword';
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -14,5 +21,67 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('hash password', () => {
+    it('should hash password', async () => {
+      (bcrypt.genSalt as jest.Mock).mockResolvedValue(salt);
+      (bcrypt.hash as jest.Mock).mockResolvedValueOnce(hashedPassword);
+
+      const result = await service.hashPassword(password);
+
+      expect(result).toEqual({
+        error: false,
+        status: 0,
+        message: 'Hashed Successful',
+        result: {
+          hashedPassword,
+        }
+      });
+      expect(bcrypt.genSalt).toHaveBeenCalledWith(10);
+      expect(bcrypt.hash).toHaveBeenCalledWith(password, salt);
+    });
+
+    it('should handle errors when hash password failed', async () => {
+      (bcrypt.genSalt as jest.Mock).mockRejectedValue({});
+
+      const result = await service.hashPassword(password);
+
+      expect(result).toEqual({
+        error: true,
+        status: 500,
+        message: 'Hash Failed',
+      });
+    });
+  });
+
+  describe('validate password', () => {
+    it('should validate a password correctly', async () => {
+      (bcrypt.compare as jest.Mock).mockResolvedValue(true);
+
+      const result = await service.validatePassword(password, hashedPassword);
+
+      expect(result).toEqual({
+        error: false,
+        status: 0,
+        message: 'Validate Successful',
+        result: {
+          isCorrect: true,
+        },
+      });
+      expect(bcrypt.compare).toHaveBeenCalledWith(password, hashedPassword);
+    });
+
+    it('should handle error when validate password failed', async () => {
+      (bcrypt.compare as jest.Mock).mockRejectedValue(true);
+
+      const result = await service.validatePassword(password, hashedPassword);
+
+      expect(result).toEqual({
+        error: true,
+        status: 500,
+        message: 'Validate Failed',
+      });
+    });
   });
 });
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,50 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  private readonly saltRounds = Number(process.env.SALT_ROUND);
+
+  async hashPassword(password: string) {
+    try {
+      const salt = await bcrypt.genSalt(this.saltRounds);
+      const hashedPassword = await bcrypt.hash(password, salt);
+
+      return {
+        error: false,
+        status: 0,
+        message: 'Hashed Successful',
+        result: {
+          hashedPassword,
+        },
+      }
+    } catch (e) {
+      return {
+        error: true,
+        status: 500,
+        message: 'Hash Failed',
+      }
+    }
+  }
+
+  async validatePassword(password: string, hashedPassword: string) {
+    try {
+      const result = await bcrypt.compare(password, hashedPassword);
+
+      return {
+        error: false,
+        status: 0,
+        message: 'Validate Successful',
+        result: {
+          isCorrect: result,
+        },
+      }
+    } catch (e) {
+      return {
+        error: true,
+        status: 500,
+        message: 'Validate Failed',
+      };
+    }
+  }
+}
